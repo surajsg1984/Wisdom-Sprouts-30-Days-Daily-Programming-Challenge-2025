fa9e121418465afce9d2885a2d105a03b2f60b46
surajsg1984 <surajsuryavanshi2003@gmail.com>
2025-08-16 13:09:38 +0530

    Create Day2.py
    Day2 Coding challange - Find the Missing Number

---
+++ Day2.py
@@ -0,0 +1,6 @@
+def find_missing_number(arr):
+    n = len(arr) + 1 
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(arr)
+    return expected_sum - actual_sum
+print(find_missing_number([1, 2, 4, 5]))        # 3
